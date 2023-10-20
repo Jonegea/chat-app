1274720d2c5ff04ac5b1ca913ac3a9de0749090e
Jonegea <jon_11_490@hotmail.com>
2023-10-20 14:37:00 -0300

    funcionando

--- public/client.js
+++ public/client.js
@@ -1,41 +1,27 @@
-// Crear una nueva conexión WebSocket con el servidor en el puerto 3000
 const socket = new WebSocket('ws://localhost:3000');
 
-// Obtener elementos del DOM
 const chatWindow = document.getElementById('chat');
 const messageForm = document.getElementById('form');
 const messageInput = document.getElementById('message');
 
-// Agregar un listener al formulario para manejar el envío de mensajes
 messageForm.addEventListener('submit', (event) => {
-    // Evitar el comportamiento predeterminado del formulario
     event.preventDefault();
-    // Obtener el mensaje del campo de entrada
     const message = messageInput.value;
-    // Enviar el mensaje al servidor a través del socket WebSocket
     if (message.trim() !== '') {
         socket.send(message);
-        // Limpiar el campo de entrada
+        displayMessage(`Tú: ${message}`); // Mostrar el mensaje enviado por el propio cliente
         messageInput.value = '';
     }
 });
 
-// Agregar un listener para manejar los mensajes entrantes desde el servidor
 socket.addEventListener('message', (event) => {
-    // Obtener el mensaje del evento
     const message = event.data;
-    // Mostrar el mensaje en la ventana del chat llamando a la función displayMessage
     displayMessage(message);
 });
 
-// Función para mostrar mensajes en la ventana del chat
 function displayMessage(message) {
-    // Crear un nuevo elemento <div> para el mensaje
     const messageElement = document.createElement('div');
-    // Establecer el texto del elemento como el mensaje recibido
     messageElement.textContent = message;
-    // Añadir el elemento al chatWindow
     chatWindow.appendChild(messageElement);
-    // Asegurar que el chat se desplace hacia abajo para mostrar el mensaje más reciente
     chatWindow.scrollTop = chatWindow.scrollHeight;
 }
--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-// Importar los módulos necesarios
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const path = require('path');
 
-// Crear una instancia de la aplicación Express
 const app = express();
-// Crear un servidor HTTP utilizando la aplicación Express
 const server = http.createServer(app);
-// Crear un servidor WebSocket pasando el servidor HTTP como argumento
 const wss = new WebSocket.Server({ server });
 
 // Configuración para servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Definir la lógica cuando un cliente se conecta al servidor a través de WebSocket
 wss.on('connection', (ws) => {
-    // Lógica de WebSocket - Esta parte está incompleta y debe ser llenada con la lógica del chat
+    // Cuando el servidor recibe un mensaje de un cliente
+    ws.on('message', (message) => {
+        // Retransmite el mensaje a todos los clientes conectados
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
 });
 
-// Iniciar el servidor en el puerto 3000 y mostrar un mensaje en la consola cuando el servidor está escuchando
 server.listen(3000, () => {
     console.log('Servidor WebSocket escuchando en el puerto 3000');
 });
